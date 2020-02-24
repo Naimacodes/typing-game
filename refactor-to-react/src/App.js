c4f6407825e706a4c6e89d0d7b581a2abfc3945f
Rory Gilmore <naima.adanahmed@gmail.com>
2020-02-24 17:15:32 +0100

    added game over screen and reload game button

--- refactor-to-react/src/App.js
+++ refactor-to-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 
 import './App.css';
 
@@ -33,24 +33,22 @@ function App() {
       ? localStorage.getItem('difficulty')
       : 'medium';
 
-  
   const [word, setWord] = useState(
     words[Math.floor(Math.random() * words.length)]
   );
   const [score, setScore] = useState(initialScore);
   const [addClass, setAddClass] = useState(false);
-  
 
   const onChange = e => {
     const insertedText = e.target.value;
     if (insertedText === word) {
       setWord(words[Math.floor(Math.random() * words.length)]);
       updateScore();
-      if (difficulty === 'hard'){
+      if (difficulty === 'hard') {
         setTimeLeft(timeLeft + 2);
-      } else if (difficulty === 'medium'){
-        setTimeLeft(timeLeft + 3)
-      }else {
+      } else if (difficulty === 'medium') {
+        setTimeLeft(timeLeft + 3);
+      } else {
         setTimeLeft(timeLeft + 2);
       }
 
@@ -58,11 +56,15 @@ function App() {
       e.target.value = '';
     }
   };
+  ////////////////////////////////////////////////////
+  //update score
 
   function updateScore() {
     setScore(prevscore => prevscore + 1);
   }
 
+  //////////////////////////////////////////////////
+  //pick the difficulty and set it to localStorage
   const pickDifficulty = e => {
     difficulty = e.target.value;
     localStorage.setItem('difficulty', difficulty);
@@ -72,36 +74,39 @@ function App() {
     setAddClass(!addClass);
   };
 
+  /////////////////////////////////
+  //set the countdown
+
   let seconds = 10;
 
   // initialize timeLeft with the seconds prop
   const [timeLeft, setTimeLeft] = useState(seconds);
 
-  
   useEffect(() => {
-    
     // exit early when we reach 0
     if (!timeLeft) return;
 
     // save intervalId to clear the interval when the
     // component re-renders
-    
-      const intervalId = setInterval(() => {
-        setTimeLeft(timeLeft - 1);
-      }, 1000);
-  
-      // clear interval on re-render to avoid memory leaks
-      return () => clearInterval(intervalId);
-      // add timeLeft as a dependency to re-rerun the effect
-      // when we update it
-    
-    
+
+    const intervalId = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+
+    // clear interval on re-render to avoid memory leaks
+    return () => clearInterval(intervalId);
+    // add timeLeft as a dependency to re-rerun the effect
+    // when we update it
   }, [timeLeft]);
 
   const difficultySelect = () =>
     localStorage.getItem('difficulty') !== null
       ? localStorage.getItem('difficulty')
       : 'medium';
+
+  const refreshPage = () => {
+    window.location.reload(false);
+  }
 
   return (
     <div className='App'>
@@ -131,7 +136,6 @@ function App() {
           autocomplete='off'
           placeholder='type the words here'
           onChange={onChange}
-          
         />
         <p className='time-container'>
           Time left: <span id='time'>{timeLeft}s</span>
@@ -139,7 +143,19 @@ function App() {
         <p className='score-container'>
           Score= {score} <span id='score'></span>
         </p>
-        <div id='end-game-container' className='end-game-container'></div>
+        <div
+          id='end-game-container'
+          className='end-game-container'
+          style={timeLeft === 0 ? { display: 'flex' } : { display: 'none' }}
+        >
+          {timeLeft === 0 && (
+            <Fragment>
+              <h1>Time ran out!</h1>
+              <p>You final score is {score}.</p>
+              <button onClick={refreshPage}>Reload</button>
+            </Fragment>
+          )}
+        </div>
       </div>
     </div>
   );
