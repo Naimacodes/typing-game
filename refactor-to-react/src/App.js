70da168c35e7f7713725cc0792575a864cbd608a
Rory Gilmore <naima.adanahmed@gmail.com>
2020-02-24 10:25:30 +0100

    toggle of settings button

--- refactor-to-react/src/App.js
+++ refactor-to-react/src/App.js
@@ -25,43 +25,68 @@ function App() {
     'drag',
     'loving'
   ];
+
+
   let initialScore = 0;
+
+  let difficulty =
+    localStorage.getItem('difficulty') !== null
+      ? localStorage.getItem('difficulty')
+      : 'medium';
+
   const [text, setText] = useState('');
   const [word, setWord] = useState(
     words[Math.floor(Math.random() * words.length)]
   );
   const [score, setScore] = useState(initialScore);
+  const [addClass, setAddClass] = useState(false)
+ 
 
   const onChange = e => {
     const insertedText = e.target.value;
     console.log(insertedText);
     if (insertedText === word) {
-      
       setWord(words[Math.floor(Math.random() * words.length)]);
       updateScore();
-      
+
       //clear
       e.target.value = '';
     }
   };
 
   function updateScore() {
-   
-    setScore(prevscore => prevscore + 1)
+    setScore(prevscore => prevscore + 1);
   }
-   
 
+  const pickDifficulty =  e => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
+  };
+
+  
+
+const toggle = () => {
+  setAddClass(!addClass);
+}
+
+ 
 
   return (
     <div className='App'>
-      <button id='settings-btn' className='settings-btn'>
+      <button id='settings-btn' onClick={toggle} className='settings-btn' >
+            {addClass ? (
+              <i className='fas fa-pause'></i>
+            ) : (
+              <i className='fas fa-play'></i>
+            )}>
         <i className='fas fa-cog'></i>
       </button>
-      <div id='settings' className='settings'>
-        <form action='' id='setting-form'>
+      <div id='settings' className={addClass ? 'settings' : 'settings hide'}>
+        <form action='' id='setting-form' onChange={pickDifficulty}>
           <div>
             <label for='difficulty'></label>
-            <select name='' id='difficulty'>
+            <select name='' id='difficulty'
+            >
               <option value='easy'>Easy</option>
               <option value='medium'>Medium</option>
               <option value='hard'>Hard</option>
