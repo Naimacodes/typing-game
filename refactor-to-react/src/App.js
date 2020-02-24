b2bf13685c6b494a063aea406fdc865b82a9364e
Rory Gilmore <naima.adanahmed@gmail.com>
2020-02-24 16:46:47 +0100

    added simple countdown and worked on the difficulty settings

--- refactor-to-react/src/App.js
+++ refactor-to-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
@@ -26,7 +26,6 @@ function App() {
     'loving'
   ];
 
-
   let initialScore = 0;
 
   let difficulty =
@@ -34,17 +33,16 @@ function App() {
       ? localStorage.getItem('difficulty')
       : 'medium';
 
-  const [text, setText] = useState('');
+  
   const [word, setWord] = useState(
     words[Math.floor(Math.random() * words.length)]
   );
   const [score, setScore] = useState(initialScore);
-  const [addClass, setAddClass] = useState(false)
- 
+  const [addClass, setAddClass] = useState(false);
+  
 
   const onChange = e => {
     const insertedText = e.target.value;
-    console.log(insertedText);
     if (insertedText === word) {
       setWord(words[Math.floor(Math.random() * words.length)]);
       updateScore();
@@ -58,35 +56,56 @@ function App() {
     setScore(prevscore => prevscore + 1);
   }
 
-  const pickDifficulty =  e => {
+  const pickDifficulty = e => {
     difficulty = e.target.value;
     localStorage.setItem('difficulty', difficulty);
   };
 
+  const toggle = () => {
+    setAddClass(!addClass);
+  };
+
+  let seconds = 10;
+
+  // initialize timeLeft with the seconds prop
+  const [timeLeft, setTimeLeft] = useState(seconds);
+
   
+  useEffect(() => {
+    
+    // exit early when we reach 0
+    if (!timeLeft) return;
 
-const toggle = () => {
-  setAddClass(!addClass);
-}
+    // save intervalId to clear the interval when the
+    // component re-renders
+    
+      const intervalId = setInterval(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+  
+      // clear interval on re-render to avoid memory leaks
+      return () => clearInterval(intervalId);
+      // add timeLeft as a dependency to re-rerun the effect
+      // when we update it
+    
+    
+  }, [timeLeft]);
 
- 
+  const difficultySelect = () =>
+    localStorage.getItem('difficulty') !== null
+      ? localStorage.getItem('difficulty')
+      : 'medium';
 
   return (
     <div className='App'>
-      <button id='settings-btn' onClick={toggle} className='settings-btn' >
-            {addClass ? (
-              <i className='fas fa-pause'></i>
-            ) : (
-              <i className='fas fa-play'></i>
-            )}>
+      <button id='settings-btn' onClick={toggle} className='settings-btn'>
         <i className='fas fa-cog'></i>
       </button>
-      <div id='settings' className={addClass ? 'settings' : 'settings hide'}>
+      <div id='settings' className={addClass ? 'settings hide' : 'settings'}>
         <form action='' id='setting-form' onChange={pickDifficulty}>
           <div>
-            <label for='difficulty'></label>
-            <select name='' id='difficulty'
-            >
+            <label htmlFor='difficulty'></label>
+            <select name='' id='difficulty'>
               <option value='easy'>Easy</option>
               <option value='medium'>Medium</option>
               <option value='hard'>Hard</option>
@@ -105,10 +124,10 @@ const toggle = () => {
           autocomplete='off'
           placeholder='type the words here'
           onChange={onChange}
-          text={text}
+          
         />
         <p className='time-container'>
-          Time left: <span id='time'>10s</span>
+          Time left: <span id='time'>{timeLeft}s</span>
         </p>
         <p className='score-container'>
           Score= {score} <span id='score'></span>
